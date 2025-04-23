30f2113b8e80c1a76638742a23757fd489821fa9
Nikhil s Reddy <168651733+Nikhil-DEV-777@users.noreply.github.com>
2025-04-23 18:10:45 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,85 @@
+let gameseq=[];
+let userseq=[];
+let allbtns=['yellow','red','purple','green'];
+let started=false;
+let level=0;
+let inx=-1;
+let btns =document.querySelectorAll('.btn');
+document.addEventListener("keypress",function(){
+    
+    if(started!=true){
+        console.log("game stated");
+        levelup();
+    }
+    started=true;
+})
+
+function levelup(){
+   let lev= document.querySelector("h4")
+    level+=1;
+    inx=-1;
+    userseq=[]
+    lev.innerText='Level = '+level;
+    let ran=Math.floor(Math.random()*4)
+    let btn=document.querySelector("."+allbtns[ran]);
+    gameseq.push(allbtns[ran]);
+    gamebtnflash(btn);
+}
+function checkans(inx){
+    if(gameseq[inx]===userseq[inx]){
+        if(userseq.length===gameseq.length){
+            setTimeout(levelup(),1000); 
+        }
+    }
+    else{
+        let body=document.querySelector("body");
+            body.style.backgroundColor='red';
+            
+        setTimeout(function(){
+            body.style.backgroundColor='white';
+        },500)
+        let lev= document.querySelector("h4")
+        lev.innerHTML=`Game Over! Your score was ${level} Press any key to restart`;
+        restart();
+
+    }
+}
+
+function gamebtnflash(num){
+    setTimeout(function(){
+        num.classList.add('flash');
+        },250)
+    setTimeout(function(){
+    num.classList.remove('flash');
+    },500)
+}
+function playerbtnflash(num){
+    num.classList.add('playerflash');
+    setTimeout(function(){
+    num.classList.remove('playerflash');
+    },200)
+}
+
+function buttonpress(){
+    let btn=this;
+    let user=btn.getAttribute("id");
+    playerbtnflash(btn);
+    userseq.push(user); 
+    
+    inx+=1;
+    checkans(inx);
+  }
+
+for(btn of btns){
+    btn.addEventListener('click',buttonpress)
+}
+   
+function restart(){
+    started=false;
+    gameseq=[];
+    userseq=[];
+    level=0;
+}
+
+
+
